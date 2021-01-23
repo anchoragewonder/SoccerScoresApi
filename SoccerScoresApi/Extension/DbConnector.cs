8f00bcc13508bd5bbb9020cece54a1416a1f33f5
anchoragewonder <anchoragewonder@gmail.com>
2021-01-22 20:43:32 -0500

    initial db connection

---
+++ SoccerScoresApi/Extension/DbConnector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MySql;
+using MySql.Data.MySqlClient;
+
+namespace SoccerScoresApi.Extension
+{
+    class DbConnector
+    {
+        protected static string Password = "Blackdog2020$";
+        protected static string Username = "Golohas";
+        protected static int Port = 3306;
+        protected static string Endpoint = "rds-mysql-lol.chzcbnejugbt.us-east-1.rds.amazonaws.com";
+        protected static string Database = "soccer_fixtures";
+
+        public MySqlConnection Connection;
+
+        public async Task<bool> IsConnected()
+        {
+            string connectionString = $"Server={Endpoint}; database={Database}; UID={Username}; password={Password}";
+            Connection = new MySqlConnection(connectionString);
+            await Connection.OpenAsync();
+            return Connection.State == System.Data.ConnectionState.Open;
+        }
+
+        public async Task Disconnect()
+        {
+            await Connection.CloseAsync();
+        }
+    }
+}
