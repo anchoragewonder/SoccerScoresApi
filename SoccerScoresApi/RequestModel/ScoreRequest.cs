ae85c1b2d69e0ead0d3009db48f8567c0519a69d
anchoragewonder <anchoragewonder@gmail.com>
2021-02-01 11:42:25 -0500

    updating table query

--- SoccerScoresApi/RequestModel/ScoreRequest.cs
+++ SoccerScoresApi/RequestModel/ScoreRequest.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using SoccerScoresApi.ResponseModel;
+using SoccerScoresApi.TableModel;
 
 namespace SoccerScoresApi.RequestModel
 {
     public class ScoreRequest
     {
+        [JsonProperty(PropertyName = "matches")]
+        public List<ScoreUpdateModel> Matches;
 
+        public ScoreRequest() { }
+
+        public ScoreRequest(List<SoccerTable> list)
+        {
+            List<ScoreUpdateModel> matches = new List<ScoreUpdateModel>();
+            foreach (SoccerTable tableInstance in list)
+            {
+                matches.Add(new ScoreUpdateModel(tableInstance));
+            }
+            this.Matches = matches;
+        }
     }
 }
