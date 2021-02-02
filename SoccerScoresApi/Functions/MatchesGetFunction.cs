e538d8ba5affdeffd57a0d5b4a3b9d87d600575f
anchoragewonder <anchoragewonder@gmail.com>
2021-02-02 10:45:22 -0500

    creation of empty get function and path parameters for yaml

--- SoccerScoresApi/Functions/MatchesGetFunction.cs
+++ SoccerScoresApi/Functions/MatchesGetFunction.cs
@@ -17,19 +17,51 @@ namespace SoccerScoresApi.Functions
 {
     public class MatchesGetFunction
     {
+        private const string EXAMPLE_TEXT = "Here is how to use my api." +
+            "Enter a premier league team name and the scores and fixtures for all of thier  matches will be generated.";
+
         public MatchesGetFunction()
         {
         }
 
         public async Task<APIGatewayProxyResponse> Execute(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
         {
+
+            if (apigProxyEvent.PathParameters == null)
+            {
+                List<ScoreUpdateModel> example = await GetScores("Manchester United");
+                return new APIGatewayProxyResponse
+                {
+                    Body = $"{EXAMPLE_TEXT}\n{JsonConvert.SerializeObject(example, Formatting.Indented)}",
+                    StatusCode = 200,
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" }, { "Access-Control-Allow-Origin", "*" } }
+                };
+            }
+
             _ = apigProxyEvent.PathParameters.TryGetValue("name", out string name);
 
             try
             {
+                List<ScoreUpdateModel> response = await GetScores(name);
 
+                return new APIGatewayProxyResponse
+                {
+                    Body = JsonConvert.SerializeObject(response, Formatting.Indented),
+                    StatusCode = 200,
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" }, { "Access-Control-Allow-Origin", "*" } }
+                };
+            }
+            catch(Exception)
+            {
+                return new APIGatewayProxyResponse
+                {
+                    Body = $"No Team found with the name: {name}",
+                    StatusCode = 403,
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" }, { "Access-Control-Allow-Origin", "*" } }
+                };
             }
         }
+    
         public async Task<List<ScoreUpdateModel>> GetScores(string name)
         {
             TableQuery newScore = new TableQuery();
